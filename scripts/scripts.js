c6e09aba3b528abebd16debba22bc1ffeffe1572
lRebornsl <anthony_vasquez98@hotmail.com>
2023-02-26 16:31:11 -0500

    Modified js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
 const items = Array.from(document.querySelectorAll('.rating li'));
+const btnSubmit = document.querySelector('.sombra');
+const divMain = document.querySelector('.main-container');
 
 items.forEach(item => {
   item.addEventListener('click', () => {
@@ -14,3 +16,24 @@ items.forEach(item => {
     }
   });
 });
+
+btnSubmit.addEventListener('click', () => {
+  divMain.innerHTML = "";
+  newMain()
+});
+
+
+
+function newMain(){
+  divMain.innerHTML = `
+  <img src="./images/illustration-thank-you.svg" alt="" class="thank-you">
+  <div class="text">
+            <p>
+                You selected 4 of 5
+            </p>
+            <h1>Thank you!</h1>
+            <p>
+              We appreciate you taking the time to give a rating. If you ever need more support, don't hestitate to get in touch!
+            </p>
+  </div>`;
+}
